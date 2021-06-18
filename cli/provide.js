5d611087326bf6bc5114388d1d577536fbeebf91
Janet Liang <jan.liang96@gmail.com>
2021-06-18 15:29:51 -0700

    [testing] Update provide + remove liquidity to use new escrow utils

--- cli/provide.js
+++ cli/provide.js
@@ -1,58 +1,36 @@
 require('dotenv').config()
 
 let {init, gasOptions, approve, toAppropriateDecimals} = require('./utils')
+let {deposit} = require('./escrow')
 
-const cashMarketJson = require('../abi/CashMarket.json')
-const escrowJson = require('../abi/Escrow.json')
+const marketABI = require('../abi/CashMarket.json')
 
-const cashMarketAddr = process.env.MARKET_ADDRESS
-const escrowAddr = process.env.ESCROW_ADDRESS
+const marketAddress = process.env.MARKET_ADDRESS
+const escrowAddress = process.env.ESCROW_ADDRESS
 const daiAddress = process.env.DAI_ADDRESS
 
-// Deposit liquidity (not BNB)
-// NOTE: BNB should work with the depositETH method?
-// TODO: Pull function into escrow utils to reduce duplication later
-async function deposit(tokenAddress, amount, web3) {
-  let escrowContract = new web3.eth.Contract(escrowJson, escrowAddr)
-
-  let convertedAmount = await toAppropriateDecimals(tokenAddress, amount, web3)
-
-  await escrowContract.methods.deposit(tokenAddress, convertedAmount).send(gasOptions(web3)).on(
-    'receipt', function (receipt) {
-      console.log('Deposit successful')
-      console.log(receipt)
-    }
-  ).on(
-    'error', function (error, receipt) {
-      console.log('Deposit failed.')
-      console.log(error)
-      process.exit()
-    }
-  )
-}
-
-
-// TODO: Figure out what these numbers should look like
-// BSC has essentially instant finality (3 second), so we can most likely ignore slippage
+// TODO: Figure out slippage numbers later
 const maxTime = 4294967295
 const minImpliedRate = 0
 const maxImpliedRate = 4294967295
 
-async function provide(tokenAddr, amount, maturity, web3) {
-  let marketContract = new web3.eth.Contract(cashMarketJson, cashMarketAddr)
+async function provide(tokenAddress, amount, maturity, web3) {
+  let marketContract = new web3.eth.Contract(marketABI, marketAddress)
 
-  let convertedAmount = await toAppropriateDecimals(tokenAddr, amount, web3)
-  // Add 1 for maxfCash param
-  let expectedfCash = await marketContract.methods.getCurrentCashTofCash(maturity, convertedAmount.toString()).call() + 1
+  await approve(tokenAddress, escrowAddress, amount, web3)
+  await deposit(tokenAddress, amount, web3)
 
-  await marketContract.methods.addLiquidity(maturity, convertedAmount.toString(), expectedfCash, minImpliedRate, maxImpliedRate, maxTime).send(gasOptions(web3)).on(
+  let convertedAmount = await toAppropriateDecimals(tokenAddress, amount, web3)
+  let fCashMax = await marketContract.methods.getCurrentCashTofCash(maturity, convertedAmount.toString()).call() + 1
+
+  await marketContract.methods.addLiquidity(maturity, convertedAmount.toString(), fCashMax, minImpliedRate, maxImpliedRate, maxTime).send(gasOptions(web3)).on(
     'receipt', function(receipt) {
-      console.log('Deposit successful')
+      console.log('Provide liquidity successful')
       console.log(receipt)
     }
   ).on(
     'error', function(error, receipt) {
-      console.log('Deposit failed.')
+      console.log('Provide liquidity failed.')
       console.log(error)
       process.exit()
     }
@@ -64,12 +42,7 @@ const provideAmount = 5
 const maturity = 1632960000
 
 init(process.env.PRIVATE_KEY).then((web3) => {
-  approve(daiAddress, escrowAddr, provideAmount, web3).then(() => {
-    // TODO: If deposit succeeds, don't deposit again
-    return deposit(daiAddress, provideAmount, web3)
-  }).then(() => {
-    return provide(daiAddress, provideAmount, maturity, web3)
-  }).then(() => {
+  provide(daiAddress, provideAmount, maturity, web3).then(() => {
     process.exit(0)
   })
 })
--- cli/remove.js
+++ cli/remove.js
@@ -1,32 +1,29 @@
 require('dotenv').config()
 
-let {init, gasOptions, approve, toAppropriateDecimals} = require('./utils')
+let {init, gasOptions, toAppropriateDecimals} = require('./utils')
 
-const cashMarketJson = require('../abi/CashMarket.json')
+const marketABI = require('../abi/CashMarket.json')
 
-const cashMarketAddr = process.env.MARKET_ADDRESS
+const marketAddress = process.env.MARKET_ADDRESS
 const daiAddress = process.env.DAI_ADDRESS
 
-// No approval required
-
-// Random timeout value (assumed seconds)
-// Could be block
+// Random timeout value
 const maxTime = 4294967295
 
 async function remove(tokenAddr, amount, maturity, web3) {
-  let marketContract = new web3.eth.Contract(cashMarketJson, cashMarketAddr)
+  let marketContract = new web3.eth.Contract(marketABI, marketAddress)
 
   let convertedAmount = await toAppropriateDecimals(tokenAddr, amount, web3)
 
   // Returns amount of cash claimed from burning liquidity token
   await marketContract.methods.removeLiquidity(maturity, convertedAmount, maxTime).send(gasOptions(web3)).on(
     'receipt', function(receipt) {
-      console.log('Withdrawal successful')
+      console.log('Remove liquidity successful')
       console.log(receipt)
     }
   ).on(
     'error', function(error, receipt) {
-      console.log('Withdrawal failed.')
+      console.log('Remove liquidity failed.')
       console.log(error)
       process.exit()
     }
